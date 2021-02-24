9b8d19fedc61889b04b548528ec026ce83395e20
BogdanovaMarina <bogdanovamariwa@gmail.com>
2021-02-25 00:33:16 +0200

    feat: add structure

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
+import React from "react"
+import "./index.css"
+import Profile from "./components/Profile/Profile"
+import Statistics from "./components/Statistics/Statistics"
 
+function App() {
+	return (
+		<>
+			<Profile />
+			<Statistics />
+		</>
+	)
+}
+export default App
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+import React from "react"
+import ReactDOM from "react-dom"
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+import "./index.css"
+import App from "./App"
+
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root"),
+)
+export default App
