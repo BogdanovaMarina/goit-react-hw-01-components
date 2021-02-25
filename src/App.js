2bd3454b336fe92466b9379a74ba1418a5811878
BogdanovaMarina <bogdanovamariwa@gmail.com>
2021-02-26 00:29:51 +0200

    feat: add done statistics component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from "react"
+import "./App.css"
 import "./index.css"
 import Profile from "./components/Profile/Profile"
+import FriendList from "./components/FriendList/FriendList"
 import Statistics from "./components/Statistics/Statistics"
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory"
 
+import user from "./db/user.json"
+import statisticalData from "./db/statistical-data.json"
+// console.log(statisticalData)
+// console.log(user)
+const { name, tag, location, avatar, stats } = user
 function App() {
 	return (
 		<>
-			<Profile />
-			<Statistics />
+			<Profile name={name} tag={tag} location={location} avatar={avatar} stats={stats} />
+			<Statistics title="Upload stats" stats={statisticalData} />
+			<FriendList />
+			<TransactionHistory />
 		</>
 	)
 }
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,12 +1,52 @@
 import React from "react"
 import s from "./Statistics.module.css"
+import PropTypes from "prop-types"
 
-const Statistics = function () {
+const Statistics = ({ title, stats }) => {
 	return (
 		<>
-			<p>Statistics</p>
+			<section className={s.statistics}>
+				{title && <h2 className={s.title}>Upload stats</h2>}
+				<ul className={s.statList}>
+					{stats.map(el => {
+						const { id, label, percentage } = el
+						return (
+							<li className={s.item} key={id}>
+								<span className={s.label}>{label}</span>
+								<span className={s.percentage}>{percentage}</span>
+							</li>
+						)
+					})}
+					{/* <li className="item">
+						<span className="label">.mp3</span>
+						<span className="percentage">14%</span>
+					</li>
+					<li className="item">
+						<span className="label">.pdf</span>
+						<span className="percentage">41%</span>
+					</li>
+					<li className="item">
+						<span className="label">.mp4</span>
+						<span className="percentage">12%</span>
+					</li> */}
+				</ul>
+			</section>
 		</>
 	)
 }
 
 export default Statistics
+
+Statistics.dafaultProps = {
+	title: "string",
+}
+Statistics.propTypes = {
+	title: PropTypes.string,
+	stats: PropTypes.arrayOf(
+		PropTypes.shape({
+			id: PropTypes.string.isRequired,
+			label: PropTypes.string.isRequired,
+			percentage: PropTypes.number.isRequired,
+		}),
+	),
+}
