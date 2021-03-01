7a7b89d081f51e558a9d442d2f7cef574e363991
BogdanovaMarina <bogdanovamariwa@gmail.com>
2021-03-01 23:41:36 +0200

    fix: fix Profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,48 +1,51 @@
-import React from "react"
-import PropTypes from "prop-types"
-import s from "./Profile.module.css"
-
+import React from 'react';
+import PropTypes from 'prop-types';
+import s from './Profile.module.css';
+// function generateColor() {
+// 	return "#" + Math.floor(Math.random() * 16777215).toString(16)
+// }
+// console.log(generateColor);
 const Profile = ({ name, tag, location, avatar, stats }) => {
-	// console.log(name, tag, location, avatar, stats)
-	const { followers, views, likes } = stats
-	return (
-		<>
-			<div className={s.profile}>
-				<div className={s.description}>
-					<img src={avatar} alt={name} width="300" className="avatar" />
-					<p className={s.name}>{name}</p>
-					<p className={s.tag}>{tag}</p>
-					<p className={s.location}>{location}</p>
-				</div>
+  // console.log(name, tag, location, avatar, stats)
+  const { followers, views, likes } = stats;
+  return (
+    <>
+      <div className={s.profile}>
+        <div className={s.description}>
+          <img src={avatar} alt={name} width="300" className="avatar" />
+          <p className={s.name}>{name}</p>
+          <p className={s.tag}>{tag}</p>
+          <p className={s.location}>{location}</p>
+        </div>
 
-				<ul className={s.stats}>
-					<li>
-						<span className={s.label}>Followers </span>
-						<span className={s.quantity}>{followers}</span>
-					</li>
-					<li>
-						<span className={s.label}>Views </span>
-						<span className={s.quantity}>{views}</span>
-					</li>
-					<li>
-						<span className={s.label}>Likes </span>
-						<span className={s.quantity}>{likes}</span>
-					</li>
-				</ul>
-			</div>
-		</>
-	)
-}
-export default Profile
+        <ul className={s.stats}>
+          <li>
+            <span className={s.label}>Followers </span>
+            <span className={s.quantity}>{followers}</span>
+          </li>
+          <li>
+            <span className={s.label}>Views </span>
+            <span className={s.quantity}>{views}</span>
+          </li>
+          <li>
+            <span className={s.label}>Likes </span>
+            <span className={s.quantity}>{likes}</span>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+export default Profile;
 
 Profile.propTypes = {
-	name: PropTypes.string.isRequired,
-	tag: PropTypes.string.isRequired,
-	location: PropTypes.string.isRequired,
-	avatar: PropTypes.string.isRequired,
-	stats: PropTypes.shape({
-		followers: PropTypes.number.isRequired,
-		views: PropTypes.number.isRequired,
-		likes: PropTypes.number.isRequired,
-	}),
-}
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
+};
