d551a70ec38adec15e2aaed88cf04da6c9e98531
BogdanovaMarina <bogdanovamariwa@gmail.com>
2021-03-01 23:46:53 +0200

    feat: add markup and style TransactionHistory

--- src/components/TransactionHistory/TransactionHistory.js
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -1,5 +1,44 @@
-import React from "react"
-const TransactionHistory = () => {
-	return <h2>TransactionHistory</h2>
-}
-export default TransactionHistory
+import React from 'react';
+import s from './TransactionHistory.module.css';
+import PropTypes from 'prop-types';
+
+const TransactionHistory = ({ items }) => {
+  return (
+    <>
+      <table className={s.transactionHistory}>
+        <thead>
+          <tr>
+            <th>Type</th>
+            <th>Amount</th>
+            <th>Currency</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {items.map(el => {
+            const { id, type, amount, currency } = el;
+            return (
+              <tr key={id}>
+                <td>{type}</td>
+                <td>{amount}</td>
+                <td>{currency}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
+};
+export default TransactionHistory;
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    }),
+  ),
+};
